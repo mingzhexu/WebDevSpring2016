ee9ccd1e30fc1aae816691fbd26e26f0ff8e6b5a
Maggie <maggiexu@ccs.neu.edu>
2016-03-28 21:16:46 -0700

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 //var upload = multer(); // for parsing multipart/form-data
-var session       = require('express-session');
+//var session       = require('express-session');
 app.use(express.cookieParser());
 app.use(express.session({secret: 'secret'}));
 app.use(bodyParser.json()); // for parsing application/json

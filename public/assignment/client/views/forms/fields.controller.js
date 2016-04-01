67e36548b2904f989b6d66293e7884bd91f47cd9
Maggie <maggiexu@ccs.neu.edu>
2016-03-31 21:19:09 -0700

    update field

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -32,24 +32,50 @@
         $scope.editField=function(field)
         {
             $scope.field = field;
+            var newField =
+            {
+                "_id" : field._id,
+                "label": field.label,
+                "type":field.type,
+                "placeholder": field.placeholder,
+                "options":field.options
+            };
+            $scope.selectedField = newField;
+
             var fieldType = field.type;
             if (fieldType == "TEXT" || fieldType == "TEXTAREA") {
                 $("#dialog-1").modal();
-
             } else if (fieldType == "OPTIONS" || fieldType == "CHECKBOXES" || fieldType == "RADIOS") {
                 $("#dialog-3").modal();
             }else {
                 $("#dialog-2").modal();
             }
-        }
+        };
 
         $scope.updateField = function(field, newfield)
         {
-            console.log("in update", field, field._id, formId);
-            console.log("in update", newfield, field._id, formId);
+            FieldService
+                .getFieldForForm(formId, $scope.selectedField._id)
+                .then(function(response){
+                    if(response.data)
+                    {
+                        $scope.field = response.data;
+                    }
+                });
+
+            console.log("in update", field);
             field.label = newfield.label;
             field.placeholder = newfield.placeholder;
-            field.options = newfield.options;
+            field.options = [];
+            if(newfield.options != null){
+                var options = newfield.options.split("\n");
+                for(var u in options){
+                    var s = options[u].split(":");
+                    console.log("S:",s);
+                    var one = {"label":s[0], "value":s[1]}
+                    field.options.push(one);
+                }
+            }
             FieldService
                 .updateField(formId, field._id, field)
                 .then(function(response){
